eb8c6c5e815e12a55118b1bb1cf7b4b0bdb46338
irbaaz <mwater196@gmail.com>
2022-09-06 20:33:25 +0530

    Completed Home Page , continue from 1:34:00

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import Header from "./Header"
+import Home from './Home';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      <Header/>
+      <Home/>
     </div>
   );
 }
 
 export default App;
+
+
+//https://mui.com/material-ui/material-icons/?query=basket
+//https://medium.com/cleverprogrammer/amazon-clone-using-react-the-ultimate-guide-fba2b36f3458
+//https://console.firebase.google.com/u/0/project/clone-234cc/settings/general/web:YjJiZTRkMjktMmRmMy00N2YwLTg2OWItNzA5OTZhYzI1M2U3
+//1:34:00
