078154232d71e69c0f8c2c439ec2a691f1f22b8b
Md Irbaaz Hussain <94373243+Irbaaz49@users.noreply.github.com>
2022-10-20 14:06:40 +0530

    Update firebase.js

--- src/Components/Firebase/firebase.js
+++ src/Components/Firebase/firebase.js
@@ -1,26 +1,24 @@
-import firebase from 'firebase/compat/app';
-import 'firebase/compat/auth';
-import 'firebase/compat/firestore';
+// import firebase from 'firebase/compat/app';
+// import 'firebase/compat/auth';
+// import 'firebase/compat/firestore';
 
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyCQnYzMypvoaHmHoHK7p2x1R9OHmi4Iy0g",
-  authDomain: "clone-234cc.firebaseapp.com",
-  projectId: "clone-234cc",
-  storageBucket: "clone-234cc.appspot.com",
-  messagingSenderId: "71591385507",
-  appId: "1:71591385507:web:7dd913784ca33e6d06a216",
-  measurementId: "G-7344BG455E"
-};
+// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// const firebaseConfig = {
+//   apiKey: "AIzaSyCQnYzMypvoaHmHoHK7p2x1R9OHmi4Iy0g",
+//   authDomain: "clone-234cc.firebaseapp.com",
+//   projectId: "clone-234cc",
+//   storageBucket: "clone-234cc.appspot.com",
+//   messagingSenderId: "71591385507",
+//   appId: "1:71591385507:web:7dd913784ca33e6d06a216",
+//   measurementId: "G-7344BG455E"
+// };
 
-const firebaseApp = firebase.initializeApp(firebaseConfig);
+// const firebaseApp = firebase.initializeApp(firebaseConfig);
 
-const db = firebaseApp.firestore();
-const auth = firebase.auth();
+// const db = firebaseApp.firestore();
+// const auth = firebase.auth();
 
-export { db, auth };
-
-
+// export { db, auth };
 
 
 
@@ -30,4 +28,6 @@ export { db, auth };
 
 
 
-//https://stackoverflow.com/questions/70445014/module-not-found-error-package-path-is-not-exported-from-package
+
+
+// //https://stackoverflow.com/questions/70445014/module-not-found-error-package-path-is-not-exported-from-package
